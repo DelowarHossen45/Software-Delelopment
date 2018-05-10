b3978a0e8a11f82ba22c6d7352f69c03ea7bd82d
nisa <jinnatunnisa32@gmail.com>
2018-05-10 10:02:03 +0600

    Added a funtion for multipline two veriable

--- LabFinal.cpp
+++ LabFinal.cpp
@@ -1,6 +1,14 @@
-#include<bits/std.c++>
+#include<bits/stdc++.h>
 using namespace std;
+void multiplication()
+{
+    double veri1,veri2;
+    cout<<endl<<"Enter Two veriable for Multiplication:";
+    cin>>veri1>>veri2;
+    cout<<endl<<"Multiplication of "<<veri1<<" and "<<veri2<<" is = "<<veri1*veri2;
+}
+
 int main()
 {
-	
+    multiplication();
 }
