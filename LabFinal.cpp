9047dc2e95919811f670f211cbd4faec4a4d51f4
Ebrahim <ebrahimali.cse.ru@gmail.com>
2018-05-10 09:49:00 +0600

    This is subtraction function by ebrahim

--- LabFinal.cpp
+++ LabFinal.cpp
@@ -1,6 +1,15 @@
 #include<bits/std.c++>
 using namespace std;
+
+void subtraction()
+{
+    double Num1,Num2,result;
+    cout<<"Enter two number for subtraction: ";
+    cin>>Num1>>Num2;
+    result=Num1-num2;
+    cout<<"This is output: "<<result<<endl;
+}
 int main()
 {
-	
+	subtraction();
 }
