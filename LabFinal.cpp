ac6e11bea0bcee9daa4a570f070a1ef6e07b0783
Shohan-Pramanik <shohan.pramanik38@gmail.com>
2018-05-10 10:00:54 +0600

    This is division function by Shohan

--- LabFinal.cpp
+++ LabFinal.cpp
@@ -1,6 +1,16 @@
-#include<bits/std.c++>
+#include<bits/stdc++.h>
 using namespace std;
+void division();
+void division()
+{
+    double number_1,number_2,result;
+    cout<<"Enter two numbers for division:";
+    cin>>number_1>>number_2;
+    result=number_1/number_2;
+    cout<<"Division of "<<number_1<<" and "<<number_2<<" is "<<result<<endl;
+}
 int main()
 {
+    division();
 	
 }
